309d24620a625897a2cba0c9599d5c76a3fc951f
Oleksandr Kovtiukh <a.kovtukh@gmail.com>
2021-06-25 14:37:51 +0300

    add first collection

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,20 @@
 module.exports = config => {
   config.addPassthroughCopy('./src/images/');
 
+  // Returns work items, sorted by display order
+  config.addCollection('work', collection => {
+    return collection
+    .getFilteredByGlob('./src/work/*.md')
+    .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
+  });
+
+  config.addCollection('featuredWork', collection => {
+    return collection
+    .getFilteredByGlob('./src/work/*.md')
+    .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1))
+    .filter(x => x.data.featured);
+  });
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
