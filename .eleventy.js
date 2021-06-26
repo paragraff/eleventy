dead3fbb9a50e3875a22510654160c5ef481d6ab
Oleksandr Kovtiukh <a.kovtukh@gmail.com>
2021-06-26 10:38:40 +0300

    add blog posts with pagination

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,12 @@ module.exports = config => {
     return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md').filter(x => x.data.featured))
   });
 
+  // Returns a collection of blog posts in reverse date order
+  config.addCollection('blog', collection => {
+    return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+  });
+
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
