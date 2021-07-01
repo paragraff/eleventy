a187357900e6446b6af3b2025ceb18bfcfa72409
Oleksandr Kovtiukh <a.kovtukh@gmail.com>
2021-07-01 13:00:07 +0300

    add sass precompiler

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,8 @@ const rssPlugin = require('@11ty/eleventy-plugin-rss');
 
 module.exports = config => {
   config.addPassthroughCopy('./src/images/');
+  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  config.setUseGitIgnore(false);
 
   // Add filters
   config.addFilter('dateFilter', dateFilter);
